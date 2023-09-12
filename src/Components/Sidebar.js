e287b76cc790f6b8c4abf29dd14cc67216a8ece0
wanjiku <annritawanjiku77@gmail.com>
2023-09-12 17:49:18 +0300

    added search bar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,10 +4,11 @@ import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function Sidebar() {
   return (
-    <div className='sidebar flex-[0.35]'>
+    <div className='sidebar flex flex-col flex-[0.35]'>
         {/* side bar header */}
         <div className='sidebar-header flex justify-between p-[20px] border-r-2 border-r-slate-200'>
             <IconButton>
@@ -26,11 +27,15 @@ function Sidebar() {
             </div>
         </div>
         {/* side bar search */}
-        <div className='sidebar-search'>
+        <div className='sidebar-search flex items-center bg-[#f6f6f6] h-[39px] p-[10px]'>
+            <div className='search-container flex items-center bg-white w-full h-9 rounded-[20px]'>
+                <SearchOutlinedIcon/>
+                <input className='border-none ml-[10px] text-xs ' placeholder='Search or start new chat' type="text" />
+            </div>
 
         </div>
         {/* side bar chat */}
-        <div className='sidebar-chart'>
+        <div className='sidebar-chat'>
 
         </div>
     </div>
