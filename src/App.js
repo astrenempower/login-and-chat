008d66b3ae113e101bdf0afebf7afc28d2425a64
wanjiku <annritawanjiku77@gmail.com>
2023-09-12 15:11:06 +0300

    set up tailwind and flex for chat app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#dadbd3] h-[100vh] grid place-items-center">
+      
+      {/* body */}
+      <div className='app-body flex bg-[#ededed] h-[90vh] w-[90vw] shadow-2xl mt-[-50px]'>
 
+      </div>
     </div>
   );
 }
