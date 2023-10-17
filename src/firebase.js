c613e0a7839293fce9628f457e6e79b9e2f1c90a
wanjiku <annritawanjiku77@gmail.com>
2023-10-17 13:58:08 +0300

    setup firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,30 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDGoj9SX7FsQKYN4TB5EEQVf1gcNWU6Wyo",
-    authDomain: "chat-19e60.firebaseapp.com",
-    projectId: "chat-19e60",
-    storageBucket: "chat-19e60.appspot.com",
-    messagingSenderId: "954502853268",
-    appId: "1:954502853268:web:1e373885dbe0603983ba81",
-    measurementId: "G-KLDMCLZT7R"
-  };
+  apiKey: "AIzaSyAUdFk8vrETscR5v19mk74I1GRmMuqapdM",
+  authDomain: "whatsapp-chat-3625f.firebaseapp.com",
+  projectId: "whatsapp-chat-3625f",
+  storageBucket: "whatsapp-chat-3625f.appspot.com",
+  messagingSenderId: "781971762419",
+  appId: "1:781971762419:web:23f81cad3a9f9bdb509b86",
+  measurementId: "G-ME7DC1G7SH"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// instance of googleauth provider
+export const provider = new GoogleAuthProvider();
