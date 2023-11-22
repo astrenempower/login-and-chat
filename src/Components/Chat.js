794f4efbf1b2e3e11d9acb49c28d359b5d073215
wanjiku <annritawanjiku77@gmail.com>
2023-11-22 20:14:02 +0300

    display message and time stamp

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -4,63 +4,80 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { IconButton } from "@mui/material";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
-import MicIcon from '@mui/icons-material/Mic';
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, orderBy, query } from "firebase/firestore";
 import Sidebar from "./Sidebar";
 
 function Chat() {
   const [image, setImage] = useState("");
   // submit button
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
   // get room id
-  const {roomId} = useParams()
+  const { roomId } = useParams();
   // keep track of room name
-  const [roomName, setRoomName] = useState("")
+  const [roomName, setRoomName] = useState("");
+  // keep track of messages
+  const [messages, setMessages] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     // get new messages every time room name changes
 
-   if(roomId){
-     const roomDocRef = doc(db, 'rooms', roomId)
-     const fetchRoomData = async () => {
-      try {
-        const roomSnapshot = await getDoc(roomDocRef);
-        if (roomSnapshot.exists()) {
-          setRoomName(roomSnapshot.data().name);
-        } else {
-          console.error("Room does not exist.");
+    if (roomId) {
+      // get room
+      const roomDocRef = doc(db, "rooms", roomId);
+      const fetchRoomData = async () => {
+        try {
+          const roomSnapshot = await getDoc(roomDocRef);
+          if (roomSnapshot.exists()) {
+            setRoomName(roomSnapshot.data().name);
+          } else {
+            console.error("Room does not exist.");
+          }
+        } catch (error) {
+          console.error("Error fetching room:", error);
         }
-      } catch (error) {
-        console.error("Error fetching room:", error);
+      };
+
+      // get messages
+      const messageDocRef = collection(roomDocRef, 'messages')
+      const q = query(messageDocRef, orderBy("timestamp", "asc"))
+      const fetchMessageData = async () => {
+        try {
+          const messageSnapshot = await getDocs(q);
+          if (!messageSnapshot.empty){
+            setMessages(messageSnapshot.docs.map((doc)=> doc.data()))
+          } else {
+            console.error("Message  does not exist.");
+          }
+        } catch (error) {
+          console.error("Error fetching room:", error);
+        }
       }
-    };
 
-    fetchRoomData();
-  }
-}, [roomId]);
-
-
+      fetchRoomData();
+      fetchMessageData(); 
+    }
+  }, [roomId]);
 
   useEffect(() => {
     // set image using random number
     setImage(Math.floor(Math.random() * 5000));
   }, [roomId]);
 
-
   const sendMessage = (e) => {
     e.preventDefault();
     // console.log("You typed >>>", input)
 
     // clean input after message
-    setInput("")
-  }
+    setInput("");
+  };
 
   return (
     <>
-      <Sidebar/>
+      <Sidebar />
       <div className="flex-[0.65] flex flex-col">
         {/* header */}
         <div className="chat-header p-[20px] flex items-center bg-[#1d003a]">
@@ -88,24 +105,43 @@ function Chat() {
             </IconButton>
           </div>
         </div>
+
         {/* body */}
         <div className="chat-body bg-cover bg-[#14012a] height: 400px flex-1 bg-repeat bg-center p-[30px]">
-
           {/* chat message */}
-          <p className={`chat-message ${true && 'chat-receiver'} relative text-xs p-[10px] bg-[#6B4EFF] rounded-xl w-fit mb-[30px] text-white`}>
-            <span className="chat-name absolute top-[-14px] font-extrabold text-[10px]" > Ciku W</span>
-            Hi Guys
-            <span className="time-stamp ml-[10px] text-[8px]" > 16:40pm</span>
-          </p>
-
+          {messages.map((message) => (
+            <p
+              className={`chat-message ${
+                true && "chat-receiver"
+              } relative text-xs p-[10px] bg-[#6B4EFF] rounded-xl w-fit mb-[30px] text-white`}
+            >
+              <span className="chat-name absolute top-[-14px] font-extrabold text-[10px]">
+                {" "}
+                {message.name}
+              </span>
+              {message.message}
+              {/* timestamp */}
+              <span className="time-stamp ml-[10px] text-[8px]">
+                {new Date(message.timestamp?.toDate()).toUTCString()}
+              </span>
+            </p>
+          ))}
         </div>
 
         {/* footer */}
         <div className="chat-footer flex justify-between items-center h-[62px] border-t-[1px] border-black bg-[#14012a]">
-          <InsertEmoticonIcon/>
+          <InsertEmoticonIcon />
           <form className="flex flex-1">
-            <input value={input} onChange={e => setInput(e.target.value)} className='flex-1 rounded-[30px] p-[10px] border-black bg-[#302642]' placeholder="Type a message" type="text" />
-            <button onClick={sendMessage} className="hidden" type="submit">Send a message</button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="flex-1 rounded-[30px] p-[10px] border-black bg-[#302642]"
+              placeholder="Type a message"
+              type="text"
+            />
+            <button onClick={sendMessage} className="hidden" type="submit">
+              Send a message
+            </button>
           </form>
           <MicIcon />
         </div>
